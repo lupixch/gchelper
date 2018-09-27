1add0159a16c4a374d023da584ea7fea746d70ed
Roland Leupi <roland.leupi@ibwag.com>
2018-09-27 11:58:35 +0200

    Source reformatting. window.js is now renderer.js. Path to electron.exe fixed.

--- main.js
+++ main.js
@@ -1,43 +1,43 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const url = require('url')
-const fs = require('fs');
+const fs = require('fs')
 
-require('electron-reload')(__dirname);
+require('electron-reload')(__dirname)
 
 let getAppVersionString = function() {
-   return app.getVersion();
+    return app.getVersion()
 }
 
 let window = null
 
 // Wait until the app is ready
 app.once('ready', () => {
-  // Create a new window
-  window = new BrowserWindow({
-    // Set the initial width to 800px
-    width: 800,
-    // Set the initial height to 600px
-    height: 600,
-    // Set the default background color of the window to match the CSS
-    // background color of the page, this prevents any white flickering
-    backgroundColor: "#D6D8DC",
-    // Don't show the window until it's ready, this prevents any white flickering
-    show: false
-  })
+    // Create a new window
+    window = new BrowserWindow({
+        // Set the initial width to 800px
+        width: 800,
+        // Set the initial height to 600px
+        height: 600,
+        // Set the default background color of the window to match the CSS
+        // background color of the page, this prevents any white flickering
+        backgroundColor: '#D6D8DC',
+        // Don't show the window until it's ready, this prevents any white flickering
+        show: false
+    })
 
-  // Load a URL in the window to the local index.html path
-  window.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+    // Load a URL in the window to the local index.html path
+    window.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
 
-  // Show window when page is ready
-  window.once('ready-to-show', () => {
-    window.setTitle('GC Helper V' + getAppVersionString());
-    window.show()
-  })
+    // Show window when page is ready
+    window.once('ready-to-show', () => {
+        window.setTitle('GC Helper V' + getAppVersionString())
+        window.show()
+    })
 
-  //window.webContents.openDevTools();
+    // window.webContents.openDevTools()
 })
--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-electron .
+node_modules\electron\dist\electron.exe .
