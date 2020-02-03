178d66df30d88ee4ce75119699694201e3ba2711
Roland Leupi <roland.leupi@ibwag.com>
2020-02-03 14:04:49 +0100

    coordlib basically added as interface between application and geolib.

--- test/libtest/test/geofunctions.js
+++ test/libtest/test/geofunctions.js
@@ -1,5 +1,8 @@
+/**
+ * Test helper to check the access to the geolib functions. Not really used for project code.
+ */
+
 const basedir = '../../..';
-//const wv = require(basedir + '/lib/word-value.js');
 const geo = require('geolib');
 
 
@@ -30,12 +33,23 @@ describe('distance()', function() {
     });
 });
 
-describe('distance()', function() {
+describe('bearing()', function() {
     it('should return 0 for no bearing', function() {
         expect(geo.getRhumbLineBearing(p1, p1) == 0);
     });
     it('should return correct value for real bearing', function() {
         // console.log("bearing: ", geo.getRhumbLineBearing(p1, p2));
         expect(Math.round(geo.getRhumbLineBearing(p1, p2))).to.equal(34);
+    });
+});
+
+describe('projection()', function() {
+    it('should return same point for no distance and bearing', function() {
+        expect(geo.computeDestinationPoint(p1, 0, 0).latitude).to.closeTo(p1.latitude, 0.01);
+        expect(geo.computeDestinationPoint(p1, 0, 0).longitude).to.closeTo(p1.longitude, 0.01);
+    });
+    it('should return correct point for real distance and bearing', function() {
+        expect(geo.computeDestinationPoint(p1, 13, 34).latitude).to.be.closeTo(p2.latitude, 0.01);
+        expect(geo.computeDestinationPoint(p1, 13, 34).longitude).to.be.closeTo(p2.longitude, 0.01);
     });
 });
