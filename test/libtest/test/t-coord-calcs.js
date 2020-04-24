e9eae37b2e49b3241a4517bfe56868a73e7aa5fc
Roland Leupi <rleupi@lupix.ch>
2020-04-24 11:34:15 +0200

    coord-calcs.js and tests added. Test are weak actually.

---
+++ test/libtest/test/t-coord-calcs.js
@@ -0,0 +1,66 @@
+const basedir = '../../..';
+const cc = require(basedir + '/lib/coord-calcs.js');
+
+
+let chai = require('chai');
+let expect = chai.expect;
+
+let assert = require('assert');
+
+let p1 = {
+    latitude: 47.123400,
+    longitude: 8.456700
+};
+
+let p2 = {
+    latitude: 47.1235,
+    longitude: 8.4568
+};
+
+
+describe('distance()', function() {
+    it('should return 0 for 2 equal points', function() {
+        expect(cc.distance(p1, p1)).to.equal(0);
+    });
+    it('should return correct value for 2 different points', function() {
+        expect(cc.distance(p1, p2)).to.equal(13);
+    });
+});
+
+describe('bearing()', function() {
+    it('should return 0 for 2 equal points', function() {
+        expect(cc.distance(p1, p1)).to.equal(0);
+    });
+});
+
+describe('distanceAndBearing()', function() {
+    it('should return 0 for 2 equal points', function() {
+        expect(cc.distanceAndBearing(p1, p1).distance).to.equal(0);
+        expect(cc.distanceAndBearing(p1, p1).bearing).to.equal(0);
+    });
+    it('should return correct value for 2 different points', function() {
+        expect(cc.distanceAndBearing(p1, p2).distance).to.equal(13);
+        expect(Math.round(cc.distanceAndBearing(p1, p2).bearing)).to.equal(34);
+    });
+});
+
+describe('projection()', function() {
+    it('with zero distance and angle should return same point', function() {
+        let p = cc.projection(p1, 0, 0); 
+        // console.log(p);
+        // console.log(p1);
+        expect(p.latitude.toFixed(5)).to.equal(p1.latitude.toFixed(5));
+        expect(p.longitude.toFixed(5)).to.equal(p1.longitude.toFixed(5));
+    });
+});
+
+describe('projection()', function() {
+    it('should return correct new point', function() {
+        let p = cc.projection(p1, 13, 34); 
+        // console.log(p1);
+        // console.log(p);
+        // console.log(p2);
+        expect(p.latitude.toFixed(5)).to.equal(p2.latitude.toFixed(5));
+        expect(p.longitude.toFixed(5)).to.equal(p2.longitude.toFixed(5));
+    });
+});
