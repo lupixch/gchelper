ad7a34356be7fbfb7815b6ed1440b8d4adc50c7e
Roland Leupi <rleupi@lupix.ch>
2020-07-23 14:09:20 +0200

    Map functionality basically added.

--- renderer.js
+++ renderer.js
@@ -1,12 +1,14 @@
 $(() => {
     const crypto = require('crypto');
     const rotx = require('rot');
-    const wv = require('./lib/word-value.js');
     const cy = require('cipherjs');
+    const L = require('leaflet');
+
+    const wv = require('./lib/word-value.js');
     const ccalc = require('./lib/coord-calcs.js');
     const Cconv = require('./lib/coord-converter.js').CoordConverter;
     const CCFormats = require('./lib/coord-converter.js').CCFormats;
-
+    
     let cconv = new Cconv();
     
     const fcOptions = {
@@ -17,7 +19,40 @@ $(() => {
     let key = "";
     let inputText = "";
     let inputTextUpper = "";
-    let p1, p2, p3, p4;
+
+    let bearing = 90;
+    let distance = 1000;
+    let p1 = { latitude: 47.493450, longitude: 8.218000};
+    let p2 = ccalc.projection(p1, distance, bearing); 
+    let p3, p4;
+
+    let map;
+    let p1Protect = false;
+    let p2Protect = false;
+    let distanceProtect = false;
+    let bearingProtect = false
+
+    $('#p1').val(cconv.asString(p1));
+    $('#p2').val(cconv.asString(p2));
+    $('#distance').val(distance);
+    $('#bearing').val(Math.round(bearing));
+
+    let updateMapAndFields = () => {
+        markerP1.setLatLng([p1.latitude, p1.longitude]);
+        markerP2.setLatLng([p2.latitude, p2.longitude]);
+        polyline.setLatLngs([[p1.latitude, p1.longitude],
+                             [p2.latitude, p2.longitude]]
+        );
+
+        if (!p1Protect) $('#p1').val(cconv.asString(p1));
+        if (!p2Protect) $('#p2').val(cconv.asString(p2));
+        if (!distanceProtect) $('#distance').val(distance);
+        if (!bearingProtect) $('#bearing').val(Math.round(bearing));
+        p1Protect = false;
+        p2Protect = false;
+        distanceProtect = false;
+        bearingProtect = false;
+    };
 
     $('#btnTobase64').click(function() {
         let txt = $('#base64encode-output').text();
@@ -109,61 +144,32 @@ $(() => {
     $('#p1').bind('input propertychange', function() {
         $('#error-text').text('');
         p1 = cconv.asPoint(this.value);
-        let bearing = parseInt($('#bearing').val(), 10) || 0;
-        let distance = parseInt(this.value, 10) || 0;
-        try {
-            p2 = ccalc.projection(p1, distance, bearing); 
-            $('#p2').val(cconv.asString(p2));
-        } catch(e) {
-            $('#error-text').text(e.message);
-        }
+        p1Protect = true;
+        onP1Changed();
     })
 
     $('#p2').bind('input propertychange', function() {
         $('#error-text').text('');
-        p1 = cconv.asPoint($('#p1').val());
         p2 = cconv.asPoint(this.value);
-        try {
-            let db = ccalc.distanceAndBearing(p1, p2);
-            $('#distance').val(db.distance);
-            $('#bearing').val(Math.round(db.bearing));
-        } catch(e) {
-            $('#error-text').text(e.message);
-        }
+        p2Protect = true;
+        onP2Changed();
     })
 
     $('#distance').bind('input propertychange', function() {
         $('#error-text').text('');
-        p1 = cconv.asPoint($('#p1').val());
-        let bearing = parseInt($('#bearing').val(), 10) || 0;
-        let distance = parseInt(this.value, 10) || 0;
-        try {
-            p2 = ccalc.projection(p1, distance, bearing); 
-            $('#p2').val(cconv.asString(p2));
-        } catch(e) {
-            console.error(e);
-            $('#error-text').text(e.message);
-        }
+        distance = parseInt(this.value, 10) || 0;
+        distanceProtect = true;
+        onDistanceChanged();
     })
 
     $('#bearing').bind('input propertychange', function() {
         $('#error-text').text('');
-        p1 = cconv.asPoint($('#p1').val());
-        let bearing = parseInt(this.value, 10) || 0;
-        let distance = parseInt($('#distance').val(), 10) || 0;
-        try {
-            p2 = ccalc.projection(p1, distance, bearing); 
-            $('#p2').val(cconv.asString(p2));
-    
-        } catch(e) {
-            $('#error-text').text(e.message);
-        }
+        bearing = parseInt(this.value, 10) || 0;
+        bearingProtect = true;
+        onBearingChanged()
     })
 
     $('input[type=radio][name=formatOptions]').change(function() {
-        p1 = cconv.asPoint($('#p1').val());
-        p2 = cconv.asPoint($('#p2').val());
-
         if (this.value == 'swiss') {
             cconv.setFormat(CCFormats.Swissgrid1903);
         }
@@ -176,12 +182,108 @@ $(() => {
         else if (this.value == 'dd') {
             cconv.setFormat(CCFormats.WGS84_dd);
         }
-
-        $('#p1').val(cconv.asString(p1));
-        $('#p2').val(cconv.asString(p2));
- 
-
+        updateMapAndFields();
     });
 
+    onP1Changed = () => {
+        try {
+            p2 = ccalc.projection(p1, distance, bearing); 
+        } catch(e) {
+            $('#error-text').text(e.message);
+        }
+        updateMapAndFields();
+    }
+
+    onP2Changed = () => {
+        try {
+            let db = ccalc.distanceAndBearing(p1, p2);
+            distance =db.distance;
+            bearing = Math.round(db.bearing);
+        } catch(e) {
+            $('#error-text').text(e.message);
+        }
+        updateMapAndFields();
+    }
+
+    onDistanceChanged = () => {
+        try {
+            p2 = ccalc.projection(p1, distance, bearing); 
+        } catch(e) {
+            console.error(e);
+            $('#error-text').text(e.message);
+        }
+        updateMapAndFields();
+    }
+
+    onBearingChanged = () => {
+        try {
+            p2 = ccalc.projection(p1, distance, bearing); 
+        } catch(e) {
+            console.error(e);
+            $('#error-text').text(e.message);
+        }
+        updateMapAndFields();
+    }
+
     $('#text-input').focus() // focus input box
+
+    map = L.map('map').setView([47.493450, 8.218000], 13);
+
+    var greenIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+    var blueIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+
+    let markerP1 = L.marker([p1.latitude, p1.longitude], {icon: greenIcon, draggable: true, title: 'P1'}).addTo(map);
+    let markerP2 = L.marker([p2.latitude, p2.longitude], {icon: blueIcon, draggable: true, title: 'P2'}).addTo(map);
+ 
+    let latlngs = [
+        [p1.latitude, p1.longitude],
+        [p2.latitude, p2.longitude]
+    ];
+    let polyline = L.polyline([
+        [p1.latitude, p1.longitude],
+        [p2.latitude, p2.longitude]], 
+        {color: 'red', weight: 1}).addTo(map);
+
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+        maxZoom: 18
+    }).addTo(map);
+
+    $('#coords-tab').focusin( () => {
+        setTimeout( () => {
+            map.invalidateSize(true); 
+        }, 500);
+    });
+
+    markerP1.on('moveend', function() {
+        let pos = markerP1.getLatLng();
+        p1.latitude = pos.lat;
+        p1.longitude = pos.lng;
+        onP1Changed();
+    });
+
+    markerP2.on('moveend', function() {
+        let pos = markerP2.getLatLng();
+        p2.latitude = pos.lat;
+        p2.longitude = pos.lng;
+        onP2Changed();
+    });
+
+    // Intial update of fields
+    updateMapAndFields();
+
 });
